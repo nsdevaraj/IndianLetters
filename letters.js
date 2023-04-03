b88684c94653e37a7973d43fe2a2655b503708e5
NS Devaraj <nsdevaraj@gmail.com>
2023-04-03 21:11:03 +0530

    color fixes

--- letters.js
+++ letters.js
@@ -75,22 +75,6 @@ function getAverageAngularVelocity() {
 
   return total / len;
 }
-function purifyColor(color) {
-  var randIndex = Math.round(Math.random() * 3);
-  color[randIndex] = 0;
-  return color;
-}
-function getRandomColor() {
-  var r = 100 + Math.round(Math.random() * 55);
-  var g = 100 + Math.round(Math.random() * 55);
-  var b = 100 + Math.round(Math.random() * 55);
-  return purifyColor([r, g, b]);
-}
-
-function getIndexedvowelLetter(n) {
-  var mainDigit = vowelLetters[n];
-  return mainDigit;
-}
 
 function addButton(n) {
   var nextLine = n / 18 > 1 ? 1 : 0;
@@ -102,7 +86,6 @@ function addButton(n) {
     opacity: 0.75
   });
   layer.add(button);
-
   button.add(new Konva.Tag({
     fill: 'black',
     lineJoin: 'round',
@@ -111,7 +94,6 @@ function addButton(n) {
     shadowOffset: 10,
     shadowOpacity: 0.5
   }));
-
   button.add(new Konva.Text({
     text: consonants[n],
     fontFamily: 'Calibri',
@@ -126,38 +108,13 @@ function addButton(n) {
 }
 
 function addWedge(n) {
-  var s = getRandomColor();
-  var reward = getIndexedvowelLetter(n);
-  var r = s[0];
-  var g = s[1];
-  var b = s[2];
+  var reward = vowelLetters[n];
+
   var angle = (2 * Math.PI) / numWedges;
-
-  var endColor = "rgb(" + r + "," + g + "," + b + ")";
-  r += 100;
-  g += 100;
-  b += 100;
-
-  var startColor = "rgb(" + r + "," + g + "," + b + ")";
-
   var wedge = new Konva.Group({
     rotation: (2 * n * Math.PI) / numWedges,
   });
-
-  var wedgeBackground = new Konva.Wedge({
-    radius: 300,
-    angle: angle,
-    fillRadialGradientStartPoint: 0,
-    fillRadialGradientStartRadius: 0,
-    fillRadialGradientEndPoint: 0,
-    fillRadialGradientEndRadius: 300,
-    fillRadialGradientColorStops: [0, startColor, 1, endColor],
-    fill: "#64e9f8",
-    fillPriority: "radial-gradient",
-    stroke: "#ccc",
-    strokeWidth: 2,
-  });
-
+  n % 2 ? startCol = '#ff0000' : startCol = '#660000';
   var wedgeBorderBackground = new Konva.Wedge({
     radius: 400,
     angle: angle,
@@ -165,17 +122,27 @@ function addWedge(n) {
     fillRadialGradientStartRadius: 300,
     fillRadialGradientEndPoint: 300,
     fillRadialGradientEndRadius: 400,
-    fillRadialGradientColorStops: [0, '#ffffff', 1, '#000000'],
-    fill: "#ffe9f8",
+    fillRadialGradientColorStops: [0, startCol, 1, '#000000'],
+    fill: "#008800", //highlight selected color
     fillPriority: "radial-gradient",
     stroke: "#ccc",
     strokeWidth: 2,
   });
-
   wedge.add(wedgeBorderBackground);
-
+  var wedgeBackground = new Konva.Wedge({
+    radius: 300,
+    angle: angle,
+    fillRadialGradientStartPoint: 0,
+    fillRadialGradientStartRadius: 0,
+    fillRadialGradientEndPoint: 0,
+    fillRadialGradientEndRadius: 300,
+    fillRadialGradientColorStops: [0, '#4433cc', 1, '#443322'],
+    fill: "#64e9f8",
+    fillPriority: "radial-gradient",
+    stroke: "#ccc",
+    strokeWidth: 2,
+  });
   wedge.add(wedgeBackground);
-
   var text = new Konva.Text({
     text: reward,
     fontFamily: "Calibri",
@@ -189,12 +156,9 @@ function addWedge(n) {
     y: 50,
     listening: false,
   });
-
   wedge.add(text);
   text.cache();
-
   wedge.startRotation = wedge.rotation();
-
   wheel.add(wedge);
 }
 function animate(frame) {
@@ -202,18 +166,15 @@ function animate(frame) {
   var angularVelocityChange =
     (angularVelocity * frame.timeDiff * (1 - angularFriction)) / 1000;
   angularVelocity -= angularVelocityChange;
-
   // activate / deactivate wedges based on point intersection
   var shape = stage.getIntersection({
     x: stage.width() / 2,
     y: 100,
   });
-
   if (controlled) {
     if (angularVelocities.length > 10) {
       angularVelocities.shift();
     }
-
     angularVelocities.push(
       ((wheel.rotation() - lastRotation) * 1000) / frame.timeDiff
     );
@@ -224,28 +185,23 @@ function animate(frame) {
     } else if (!finished && !controlled) {
       if (shape) {
         var text = shape.getParent().findOne("Text").text();
-        var price = text.split("\n").join("");
-        vowelLetter = price;
+        vowelLetter = text;
         vowelSign = vowelSigns[vowelLetters.indexOf(vowelLetter)];
-
         alert(consonant + " + " + vowelLetter + " = " + consonant + vowelSign)
       }
       finished = true;
     }
   }
   lastRotation = wheel.rotation();
-
   if (shape) {
     if (shape && (!activeWedge || shape._id !== activeWedge._id)) {
       pointer.y(20);
-
       new Konva.Tween({
         node: pointer,
         duration: 0.3,
         y: 30,
         easing: Konva.Easings.ElasticEaseOut,
       }).play();
-
       if (activeWedge) {
         activeWedge.fillPriority("radial-gradient");
       }
@@ -277,7 +233,7 @@ function init() {
     fillRadialGradientStartRadius: 0,
     fillRadialGradientEndPoint: 0,
     fillRadialGradientEndRadius: 30,
-    fillRadialGradientColorStops: [0, "white", 1, "red"],
+    fillRadialGradientColorStops: [0, "#885500", 1, "#443300"],
     stroke: "white",
     strokeWidth: 2,
     lineJoin: "round",
@@ -333,7 +289,6 @@ function init() {
         var atan = Math.atan(y / x);
         var rotation = x >= 0 ? atan : atan + Math.PI;
         var targetGroup = target.getParent();
-
         wheel.rotation(
           rotation - targetGroup.startRotation - target.angle() / 2
         );
@@ -341,13 +296,10 @@ function init() {
     },
     false
   );
-
   var anim = new Konva.Animation(animate, layer);
-
-  // wait one second and then spin the wheel
   setTimeout(function () {
     anim.start();
-  }, 1000);
+  }, 500);
 }
 
 function setCurrentLang(dropdown) {
