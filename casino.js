88072bd69f6ff1f786c8128c64ca1bb3d2253b74
NS Devaraj <nsdevaraj@gmail.com>
2023-04-09 18:05:03 +0530

    tamilspeech

--- casino.js
+++ casino.js
@@ -1,5 +1,5 @@
 var vowelLetters, vowelSigns, consonants, consonant, vowelLetter, vowelSign, consonantPhs, vowelLetterPhs, consonantPh, vowelLetterPh, consonantIndex;
-var width, height, centerText ;
+var width, height, centerText;
 var currentLang = 0;
 var prevletter;
 Konva.angleDeg = false;
@@ -10,12 +10,12 @@ var controlled = false;
 var numWedges = vowelLetterLangs[0].length;
 var angularFriction = 0.2;
 var target, activeWedge, stage, layer, wheel, pointer;
-var meyEzuthu='்'
-var finished = false; 
+var meyEzuthu = '்'
+var finished = false;
 function assignLanguage() {
   document.getElementById('consonDiv').innerHTML = '';
   width = window.innerWidth;
-  height = window.innerHeight; 
+  height = window.innerHeight;
   vowelLetters = vowelLetterLangs[currentLang][0];
   vowelSigns = vowelSignLangs[currentLang];
   consonants = consonantLangs[currentLang][0];
@@ -29,40 +29,40 @@ function assignLanguage() {
 
 function getAverageAngularVelocity() {
   var total = 0;
-  var len = angularVelocities.length; 
+  var len = angularVelocities.length;
   if (len === 0) {
     return 0;
-  } 
+  }
   for (var n = 0; n < len; n++) {
     total += angularVelocities[n];
-  } 
+  }
   return total / len;
 }
 
-function addButton(n) { 
-  const div = document.createElement('div'); 
+function addButton(n) {
+  const div = document.createElement('div');
   div.className = 'letter';
-  div.innerHTML =`<input id="`+n+`" type="button" class="btn" value="`+consonants[n]+`" onclick="selectConsonant(this)"  />` 
+  div.innerHTML = `<input id="` + n + `" type="button" class="btn" value="` + consonants[n] + `" onclick="selectConsonant(this)"  />`
   document.getElementById('consonDiv').appendChild(div);
-  if(n==0){
+  if (n == 0) {
     prevletter = div.childNodes[0];
     div.childNodes[0].style.backgroundColor = "#88a119";
   }
 }
 
-function selectConsonant(letter){  
+function selectConsonant(letter) {
   consonantIndex = parseInt(letter.id)
   consonant = letter.value;
   showResult()
-  if(prevletter)prevletter.style.backgroundColor= "#c8a119"
+  if (prevletter) prevletter.style.backgroundColor = "#c8a119"
   letter.style.backgroundColor = "#88a119";
-  prevletter = letter; 
+  prevletter = letter;
 }
 
 function addWedge(n) {
   var vowel = vowelLetters[n];
-  var circleRadius= stage.width()/5
-  var innerCircleRadius = circleRadius - circleRadius/4
+  var circleRadius = stage.width() / 5
+  var innerCircleRadius = circleRadius - circleRadius / 4
   var angle = (2 * Math.PI) / numWedges;
   var wedge = new Konva.Group({
     rotation: (2 * n * Math.PI) / numWedges,
@@ -70,12 +70,12 @@ function addWedge(n) {
   n % 2 ? startCol = '#ff0000' : startCol = '#660000';
   n % 2 ? startBgCol = '#980044' : startBgCol = '#7433cc';
   var wedgeBorderBackground = new Konva.Wedge({
-    radius: circleRadius, 
+    radius: circleRadius,
     angle: angle,
-    fillRadialGradientStartPoint: innerCircleRadius, 
-    fillRadialGradientStartRadius: innerCircleRadius, 
-    fillRadialGradientEndPoint: innerCircleRadius,  
-    fillRadialGradientEndRadius: circleRadius, 
+    fillRadialGradientStartPoint: innerCircleRadius,
+    fillRadialGradientStartRadius: innerCircleRadius,
+    fillRadialGradientEndPoint: innerCircleRadius,
+    fillRadialGradientEndRadius: circleRadius,
     fillRadialGradientColorStops: [0, startCol, 1, '#000000'],
     fill: "#008800", //highlight selected color
     fillPriority: "radial-gradient",
@@ -84,12 +84,12 @@ function addWedge(n) {
   });
   wedge.add(wedgeBorderBackground); // outer text circle 
   var wedgeBackground = new Konva.Wedge({
-    radius: innerCircleRadius,  
+    radius: innerCircleRadius,
     angle: angle,
     fillRadialGradientStartPoint: 0,
     fillRadialGradientStartRadius: 0,
     fillRadialGradientEndPoint: 0,
-    fillRadialGradientEndRadius: innerCircleRadius,  
+    fillRadialGradientEndRadius: innerCircleRadius,
     fillRadialGradientColorStops: [0, startBgCol, 1, '#773344'],
     fill: "#64e9f8",
     fillPriority: "radial-gradient",
@@ -98,7 +98,7 @@ function addWedge(n) {
   });
   wedge.add(wedgeBackground);// 1st inner circle 
   var wedgeBackground = new Konva.Wedge({
-    radius: (circleRadius/2)+20, 
+    radius: (circleRadius / 2) + 20,
     angle: angle,
     fill: "#443344",
     stroke: "#ccc",
@@ -114,8 +114,8 @@ function addWedge(n) {
     stroke: "yellow",
     strokeWidth: 1,
     rotation: (Math.PI + angle) / 2,
-    x: circleRadius -10,  
-    y: circleRadius/8, 
+    x: circleRadius - 10,
+    y: circleRadius / 8,
     listening: false,
   });
   wedge.add(text);
@@ -123,10 +123,22 @@ function addWedge(n) {
   wedge.startRotation = wedge.rotation();
   wheel.add(wedge);
 }
-function speak(text){
-  var msg = new SpeechSynthesisUtterance(text);
-  window.speechSynthesis.speak(msg); 
-} 
+function speak(letter1, letter2, conIndex, vowIndex) {
+  if (currentLang != 0) {
+    var msg = new SpeechSynthesisUtterance(letter1 + "+" + letter2);
+    window.speechSynthesis.speak(msg);
+  } else {
+    consonantLetter = consonants[conIndex];
+    vowelLetter = vowelLetters[vowIndex];
+    vowelSignLetter = vowelSigns[vowIndex];
+    mixedText = consonantLetter + '்' + ' plus ' + vowelLetter + '. ' + consonantLetter + vowelSignLetter
+    playAudio(mixedText)
+
+  }
+}
+function playAudio(text) {
+  new Audio("audio/" + text + ".mp3").play()
+}
 function animate(frame) {
   // handle wheel spin
   var angularVelocityChange =
@@ -134,8 +146,8 @@ function animate(frame) {
   angularVelocity -= angularVelocityChange;
   // activate / deactivate wedges based on point intersection
   var shape = stage.getIntersection({
-    x: stage.width()*0.7-20,
-    y: stage.height()/2-100, // length to detect collision
+    x: stage.width() * 0.7 - 20,
+    y: stage.height() / 2 - 100, // length to detect collision
   });
   if (controlled) {
     if (angularVelocities.length > 10) {
@@ -160,11 +172,11 @@ function animate(frame) {
   lastRotation = wheel.rotation();
   if (shape) {
     if (shape && (!activeWedge || shape._id !== activeWedge._id)) {
-      pointer.y(stage.height()/2-90);
+      pointer.y(stage.height() / 2 - 90);
       new Konva.Tween({
         node: pointer,
         duration: 0.3,
-        y: stage.height()/2-92,
+        y: stage.height() / 2 - 92,
         easing: Konva.Easings.ElasticEaseOut,
       }).play();
       if (activeWedge) {
@@ -176,16 +188,16 @@ function animate(frame) {
   }
 }
 
-function showResult(){
+function showResult() {
   var vowelIndex = vowelLetters.indexOf(vowelLetter)
-  vowelSign = vowelSigns[vowelIndex]; 
-  consonantPh= consonantPhs[consonantIndex];
-  vowelLetterPh= vowelLetterPhs[vowelIndex];
-  speak(consonantPh+ "+" +vowelLetterPh );
-  centerText.text( consonant + vowelSign)
+  vowelSign = vowelSigns[vowelIndex];
+  consonantPh = consonantPhs[consonantIndex];
+  vowelLetterPh = vowelLetterPhs[vowelIndex];
+  speak(consonantPh, vowelLetterPh, consonantIndex, vowelIndex);
+  centerText.text(consonant + vowelSign)
 }
 
-function setPointer(){
+function setPointer() {
   pointer = new Konva.Wedge({
     fillRadialGradientStartPoint: 0,
     fillRadialGradientStartRadius: 0,
@@ -197,15 +209,15 @@ function setPointer(){
     lineJoin: "round",
     angle: 1,
     radius: 30,
-    x: stage.width()*0.7-20,
-    y: stage.height()/2-100,
+    x: stage.width() * 0.7 - 20,
+    y: stage.height() / 2 - 100,
     rotation: 200,
     shadowColor: "black",
     shadowOffsetX: 3,
     shadowOffsetY: 3,
     shadowBlur: 2,
     shadowOpacity: 0.5,
-  }); 
+  });
 }
 
 function init() {
@@ -230,7 +242,7 @@ function init() {
 
   var circle = new Konva.Circle({
     x: stage.width() / 2,
-    y: stage.height() / 2 -50,
+    y: stage.height() / 2 - 50,
     radius: 120,
     fill: '#670000',
     stroke: 'black',
@@ -241,9 +253,9 @@ function init() {
     fontFamily: "Calibri",
     fontSize: 50,
     fill: "white",
-    align: "center", 
-    x: stage.width() / 2 -20,
-    y: stage.height() / 2 -80,
+    align: "center",
+    x: stage.width() / 2 - 20,
+    y: stage.height() / 2 - 80,
     listening: false,
   });
   // add components to the stage
@@ -251,7 +263,7 @@ function init() {
   layer.add(pointer);
   layer.add(circle);
   layer.add(centerText);
-  stage.add(layer); 
+  stage.add(layer);
   // bind events
   wheel.on("mousedown touchstart", function (evt) {
     angularVelocity = 0;
@@ -264,16 +276,16 @@ function init() {
     "mouseup touchend",
     function () {
       controlled = false;
-      angularVelocity = getAverageAngularVelocity() * 5; 
+      angularVelocity = getAverageAngularVelocity() * 5;
       if (angularVelocity > 20) {
         angularVelocity = 20;
       } else if (angularVelocity < -20) {
         angularVelocity = -20;
-      } 
+      }
       angularVelocities = [];
     },
     false
-  ); 
+  );
   stage.addEventListener(
     "mousemove touchmove",
     function (evt) {
@@ -297,7 +309,7 @@ function init() {
   }, 500);
 }
 
-window.onresize = function (event) { 
+window.onresize = function (event) {
   init();
 };
 
